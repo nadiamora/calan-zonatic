865c5863817fbcceb7a69a6e04eb2bd2e4043827
Angel GR <angel.13688@gmail.com>
2019-08-31 10:33:26 -0500

    Funcionalidad de editar perfil

--- zonatic/zonatic/card.php
+++ zonatic/zonatic/card.php
@@ -5,13 +5,13 @@
 		<div class="col-md-12 subarsubar">
 							
 				<div class="card-body">
-					<img src="img/estudiantes.jpg"<?php echo $articulo['imagen']; ?>" width="auto" height="170"
+					<img src="img/<?php echo $articulo['imagen']; ?>" width="auto" height="170"
 					class="card-img-top" alt="No se pudo Encontrar el Artículo"/>
 								<h5 class="card-title"><?php echo $articulo['titulo']; ?></h5>
 								<p class="card-text"><?php echo $articulo['resena']; ?></p>
 								<ul class="navbar-nav mr-auto">
 									<li class="nav-item active"><a class="nav-link"
-										href="articulo"><button
+										href="index/articulo.php"><button
 												class="enlacearticulo" type="submit">Ir a</button></a></li>
 								</ul>
 				</div>
